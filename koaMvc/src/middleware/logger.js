763df75df2a82723ae42c901565b2809efd15e6b
wangjunkai <wangjunkai@papayamobile.com>
2024-01-28 21:22:52 +0800

    delete

--- koaMvc/src/middleware/logger.js
+++
@@ -1,6 +0,0 @@
-const logger = async function (ctx, next) {
-  let stime = new Date().toISOString();
-  console.log(stime, ctx.method, ctx.host + ctx.url);
-  await next();
-};
-module.exports = () => logger;
