84250bcc06672b279afb0df8d4787d937718db61
wangjunkai <wangjunkai@papayamobile.com>
2024-04-20 00:51:01 +0800

    feat: 增加Vue2+ele UI组件

--- packages/cli/src/constant/index.js
+++ packages/cli/src/constant/index.js
@@ -10,7 +10,8 @@ const projectTypeMap={
     '基于webpack的工具库项目':'https://github.com/1623311678/utils-webpack.git',
     '基于node-koa以及mysql的mvc服务端项目':'https://github.com/1623311678/utils-webpack.git',
     '基于webpack的工具库项目':'https://github.com/1623311678/koa-mvc.git',
-    '基于react框架webview技术的vscode插件模版':'https://github.com/1623311678/vscode-webview-react.git'
+    '基于react框架webview技术的vscode插件模版':'https://github.com/1623311678/vscode-webview-react.git',
+    '基于Rollup+Vue2.0+Element-UI可按需引入的业务组件库模拟':'https://github.com/1623311678/hundun-ui-library-vue.git'
 
 }
 module.exports={
